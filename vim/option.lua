84c637d1766a241526ff708c7c2ca23d9d9b6559
Zach Valenta <zach.valenta@unitedmasters.com>
2023-11-21 17:07:38 -0500

    vim: add Rigel theme

--- vim/option.lua
+++ vim/option.lua
@@ -1,6 +1,8 @@
 local options = {
     cursorline = true,         -- highlight the current line
     relativenumber = true,     -- set relative numbered lines
+    termguicolors = true,
+    colorscheme = rigel,
 }
 
 for k, v in pairs(options) do
--- vim/plugin.vim
+++ vim/plugin.vim
@@ -1,4 +1,5 @@
 call plug#begin()
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.4' }
+Plug 'Rigellute/rigel'
 call plug#end()
