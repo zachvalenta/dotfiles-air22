2781bbcfde05ccd4d0ecce257b6c67fe218e1200
Zach Valenta <zach.valenta@unitedmasters.com>
2023-11-21 19:38:55 -0500

    nvim: telescope - layout

--- vim/init.lua
+++ vim/init.lua
@@ -1,4 +1,7 @@
+-- core
 require "zv.keymap"
 require "zv.option"
 
+-- plugins
 vim.cmd [[source ~/.config/nvim/lua/zv/plugin.vim]]
+require "zv.telescope"
---
+++ vim/telescope.lua
@@ -0,0 +1,13 @@
+require("telescope").setup{
+    defaults = {
+        layout_config = {
+            height = 0.99,
+            width = 0.99,
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.7,
+                results_width = 0.3,
+            },
+        },
+    }
+}
