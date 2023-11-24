a3df6f3b70cb65a724be7598d373b7945eb7e01f
Zach Valenta <zach.valenta@unitedmasters.com>
2023-11-24 14:48:11 -0500

    nvim: telescope - update find_files/ live_grep for workspaces

--- vim/keymap.lua
+++ vim/keymap.lua
@@ -4,5 +4,5 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>",  opts)
-keymap("n", "<leader>g", "<cmd>Telescope live_grep<cr>",  opts)
+keymap("n", "<leader>f", "<cmd>Telescope find_files follow=true<cr>",  opts)
+keymap("n", "<leader>g", "<cmd>Telescope live_grep follow=true<cr> ",  opts)
