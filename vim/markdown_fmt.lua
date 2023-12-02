f0c5fdef9236c6fad712c66b0933a13f5633fd07
Zach Valenta <zach.valenta@unitedmasters.com>
2023-12-02 17:48:02 -0500

    vim: autocommand for list text

--- vim/markdown_fmt.lua
+++ vim/markdown_fmt.lua
@@ -1,3 +1,4 @@
+-- comment text
 vim.api.nvim_exec([[
   augroup markdown_comment
     autocmd!
@@ -5,5 +6,15 @@ vim.api.nvim_exec([[
       \ syntax match mdcomment /^>.*$/ |
       \ highlight default link mdcomment Italic |
       \ highlight mdcomment guifg=#f9e2af gui=italic
+  augroup END
+]], false)
+
+-- list text
+vim.api.nvim_exec([[
+  augroup markdown_highlight_asterisk
+    autocmd!
+    autocmd FileType markdown
+      \ syntax match markdownAsterisk /^\*\s.*$/ |
+      \ highlight markdownAsterisk ctermfg=red guifg=#FF0000
   augroup END
 ]], false)
