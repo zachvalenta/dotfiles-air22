fa326ca381a2cbdcf66f1244e7a17092e80ad90e
Zach Valenta <zach.valenta@unitedmasters.com>
2023-12-14 10:14:01 -0500

    vim: smartcase

--- vim/option.lua
+++ vim/option.lua
@@ -1,6 +1,8 @@
 local options = {
     cursorline = true,         -- highlight cursor line
     relativenumber = true,     -- relative line number
+    ignorecase = true,         -- need to set for smartcase
+    smartcase = true,          -- same as rg i.e. insensitive if query lowercase, sensitive if upper
 }
 
 for k, v in pairs(options) do
